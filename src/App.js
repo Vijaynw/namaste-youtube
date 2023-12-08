73570e31749c0c439ceb334edf3ba202350442c5
Vijay <vijaywaghmare713@.com>
2023-12-08 12:01:56 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl'>Hello World</h1>
+     
+    {/* Head
+        Body
+          sidebar
+          maincontainer
+            buttonlist
+            videocontainer
+              videocard */}
+    </div>
+  );
+}
+
+export default App;
