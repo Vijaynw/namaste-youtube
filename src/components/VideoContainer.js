4d34728079de9f357b12a4aa0bcfa4d9be7219d6
Vijay <vijaywaghmare713@.com>
2023-12-08 14:57:37 +0530

    API

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import VideoCard from './VideoCard '
+import { YOUTUBE_VIDEO_APi } from '../utils/consts'
 
 const VideoContainer = () => {
+
+
+    const getdata = async () =>{
+
+        const data = await fetch(YOUTUBE_VIDEO_APi)
+        const json = await data.json()
+
+        console.log(json)
+    }
+    useEffect(()=>{
+        getdata()
+    },[])
+
   return (
-    <div>VideoContainer</div>
+    <div>VideoContainer
+        <VideoCard />
+    </div>
   )
 }
 
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,1 +1,3 @@
-const API_KEY ="AIzaSyALjHuzsbFCJwr3FaXDij066Hqe7vTxNEU"
+export const API_KEY ="AIzaSyALjHuzsbFCJwr3FaXDij066Hqe7vTxNEU"
+export const YOUTUBE_VIDEO_APi ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key="+API_KEY
+
