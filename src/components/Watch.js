6c15f5e517e6714cb623c2058ea70b2454f3198c
Vijay <vijaywaghmare713@.com>
2023-12-08 20:06:56 +0530

    Adding Redux

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -5,7 +5,16 @@ const Watch = () => {
   const [serch]=useSearchParams()
   console.log(serch.get('v'))
   return (
-    <div>{serch.get('v')}</div>
+    <div className='p-4 m-4'>
+      <div className='p-4 bg-slate-400'>
+
+      <iframe width="1200" height="550" className='rounded-xl shadow-lg' 
+      src={"https://www.youtube.com/embed/"+serch.get('v')+"?&autoPlay=1&mute=1&controls=1"}
+      title="YouTube video player" frameBorder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowFullScreen></iframe>
+      </div>
+      </div>
   )
 }
 
